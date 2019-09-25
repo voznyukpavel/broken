918640da9c5f6db844228ff7ca49ff0ad210b18b
pavel.vozniuk <opcl542GKQM!(^>
2019-09-25 19:05:48 +0300

    p

--- com.vogella.dnd.jface1/src/com/vogella/dnd/jface1/Application.java
+++ com.vogella.dnd.jface1/src/com/vogella/dnd/jface1/Application.java
@@ -18,7 +18,7 @@ public class Application implements IApplication {
 			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
 			if (returnCode == PlatformUI.RETURN_RESTART) {
 				return IApplication.EXIT_RESTART;
-			}
+			}//
 			return IApplication.EXIT_OK;
 		} finally {
 			display.dispose();
